461bc1b9699a2c7548a03e6e2c830a38fef4d03d
shenbeo <vinhnbt26@gmail.com>
2023-07-02 09:38:16 +0700

    first commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ dotenv.config()
 const app = express();
 app.use(cors(
     {
-        origin: ["https://pnv-store.vercel.app"],
+        origin: ["https://pnv-vespa.vercel.app"],
         methods: ["POST", "GET", "PUT", "DELETE"],
         credentials: true
     }
