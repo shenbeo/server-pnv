4d5c367de3f1d4181dff940c2a25e8a6801a62ad
shenbeo <vinhnbt26@gmail.com>
2023-07-03 13:50:44 +0700

    first commit

--- server.js
+++ server.js
@@ -237,7 +237,7 @@ app.post('/loginClient', (req, res) => {
                 if(response) {
                     const token = jwt.sign({role: "user_client", id: result[0].id}, "jwt-secret-key", {expiresIn: '1d'});
                     res.cookie('token', token);
-                    return res.json({Status: "Success", id: result[0].id, Error: "I am verifying your login account, please wait a few minutes"})
+                    return res.json({Status: "Success", id: result[0].id, Statuss: "I am verifying your login account, please wait a few minutes"})
                 } else {
                     return res.json({Status: "Error", Error: "Wrong Email or Password"});
                 }
